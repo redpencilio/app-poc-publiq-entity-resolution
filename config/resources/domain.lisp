34c0c80fd4684eaf2cd7d25b40f23849aa223f92
Ruben <ruben.de.facq@redpencil.io>
2025-01-05 14:43:12 +0100

    add mapping, address and location entities to resources/sparql-parser/dispatcher

---
+++ config/resources/domain.lisp
@@ -0,0 +1,11 @@
+(in-package :mu-cl-resources)
+
+(setf *include-count-in-paginated-responses* t)
+(setf *supply-cache-headers-p* t)
+(setf sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+(setf *cache-model-properties-p* t)
+(setf mu-support::*use-custom-boolean-type-p* nil)
+(setq *cache-count-queries-p* t)
+(setf sparql:*query-log-types* nil) ;; hint: use app-http-logger for logging queries instead, all is '(:default :update-group :update :query :ask)
+
+(read-domain-file "domain.json")
